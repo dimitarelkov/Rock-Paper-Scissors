7c4f501df24895cd07edf0d577a300cf999bc8df
Dimitar Atanasov <121234946+dimitarelkov@users.noreply.github.com>
2023-01-23 14:34:46 +0200

    Create RockPaperScissors_01.java

---
+++ RockPaperScissors_01.java
@@ -0,0 +1,14 @@
+package PrcaticalProjects;
+
+import java.util.Scanner;
+
+public class RockPaperScissors_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String rock = "Rock";
+        String paper = "Paper";
+        String scissors = "Scissors";
+
+    }
+}
